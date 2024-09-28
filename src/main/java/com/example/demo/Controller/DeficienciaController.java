68140a2ad74b5333344a34423e96cfa2e3131c28
Flavio Fernandes <flavio_lf_vieira@estudante.sesisenai.org.br>
2024-09-27 21:45:16 -0300

    Updates:27/09

--- src/main/java/com/example/demo/Controller/DeficienciaController.java
+++ src/main/java/com/example/demo/Controller/DeficienciaController.java
@@ -2,6 +2,7 @@ package com.example.demo.Controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,13 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.Form.DeficienciaForm;
+import com.example.demo.Form.Pessoa.PessoaForm;
 import com.example.demo.Model.Categoria;
 import com.example.demo.Model.Deficiencia;
+import com.example.demo.Model.Pessoa;
 import com.example.demo.Repository.CategoriaRepository;
 import com.example.demo.Repository.DeficienciaRepository;
 
@@ -53,9 +57,19 @@ public class DeficienciaController {
             return "deficiencia/criar";
         }
 
-        deficienciaRepository.save(deficiencia);
+        deficienciaRepository.save(deficienciaForm);
         redirectAttributes.addFlashAttribute("successMessage", "Deficiência salva com sucesso!");
         return "redirect:/deficiencias";
     }
-    
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Long id, Model model){
+        Optional<Pessoa> pessoa = pessoaRepository.findById(id);
+
+        PessoaForm pessoaForm = new PessoaForm(pessoa.get());
+
+        model.addAttribute("pessoaForm", pessoaForm);
+        model.addAttribute("id", pessoa.get().getId());
+
+        return "/pessoa/update";
+    }
 }
